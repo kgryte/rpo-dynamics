2f5c871cf080b7d6accce00f448521cc9ba2e2df
kgryte <kgryte@gmail.com>
2014-05-25 21:34:38 -0700

    [UPDATE] cleaned KDE stream pipeline.

--- app/utils/streams/stats/kde/index.js
+++ app/utils/streams/stats/kde/index.js
@@ -51,10 +51,47 @@
 		transformer = require( './../../json/transform.js' ),
 
 		// JSON stream stringify:
-		stringify = require( './../../json/stringify.js' ),
+		stringifier = require( './../../json/stringify.js' ),
 
 		// Module to calculate the KDE:
 		KDE = require( './kde.js' );
+
+
+	// FUNCTIONS //
+
+	/**
+	* FUNCTION: stringify()
+	*	Returns a transform function to stringify streamed data.
+	*/
+	function stringify() {
+		/**
+		* FUNCTION: transform( data )
+		*	Defines the data transformation.
+		*
+		* @param {array} data - streamed data
+		* @returns {string} stringified data
+		*/
+		return function transform( data ) {
+			return JSON.stringify( data );
+		}; // end FUNCTION transform()
+	} // end FUNCTION stringify()
+
+	/**
+	* FUNCTION: parse()
+	*	Returns a transform function to parse JSON streamed data.
+	*/
+	function parse() {
+		/**
+		* FUNCTION: transform( data )
+		*	Defines the data transformation.
+		*
+		* @param {array} data - streamed data
+		* @returns {string} parsed JSON data
+		*/
+		return function transform( data ) {
+			return JSON.parse( data );
+		}; // end FUNCTION transform()
+	} // end FUNCTION parse()
 
 
 	// STREAM //
@@ -85,9 +122,8 @@
 		* @returns {number} transformed data
 		*/
 		return function transform( data ) {
-			data = JSON.parse( data );
 			kde.estimator( data, 'silverman' );
-			return JSON.stringify( kde.eval( data ) );
+			return kde.eval( data );
 		};
 	}; // end METHOD transform()
 
@@ -96,26 +132,34 @@
 	*	Returns a JSON data transform stream for calculating the statistic.
 	*/
 	Stream.prototype.stream = function() {
-		var iStream, sStream, oStream, pStream;
+		var iStream, sStream, pStream, tStream, oStream, pipeline;
 
 		// Create an input transform stream: (require the data to be a single JSON chunk)
-		iStream = stringify();
+		iStream = stringifier();
 
 		// Create a sink stream to buffer all transformed data into memory:
 		sStream = eventStream.wait();
 
-		// Create an output transform stream:
-		oStream = transformer( this.transform() );
+		// Create a transform stream to parse the JSON data:
+		pStream = transformer( parse() );
+
+		// Create a transform stream to calculate the KDE:
+		tStream = transformer( this.transform() );
+
+		// Create a transform stream to stringify the output:
+		oStream = transformer( stringify() );
 
 		// Create a stream pipeline:
-		pStream = eventStream.pipeline(
+		pipeline = eventStream.pipeline(
 			iStream,
 			sStream,
+			pStream,
+			tStream,
 			oStream
 		);
 
 		// Return the pipeline:
-		return pStream;
+		return pipeline;
 	}; // end METHOD stream()
 
 
