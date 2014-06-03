c527cade8b3e3d77d528d9a94b01cdafe406727d
kgryte <kgryte@gmail.com>
2014-06-02 22:51:40 -0700

    [UPDATE] matrix figure.

---
+++ app/modules/matrix/charts/multipanel.js
@@ -0,0 +1,130 @@
+/**
+*
+*	CHART: multipanel
+*
+*
+*
+*	DESCRIPTION:
+*		- 
+*
+*
+*	API:
+*		- 
+*
+*
+*	NOTES:
+*		[1] 
+*
+*
+*	TODO:
+*		[1] 
+*
+*
+*	HISTORY:
+*		- 2014/04/25: Created. [AReines].
+*
+*
+*	DEPENDENCIES:
+*		[1] 
+*
+*
+*	LICENSE:
+*		
+*
+*	Copyright (c) 2014. Athan Reines.
+*
+*
+*	AUTHOR:
+*		Athan Reines. athan@nodeprime.com. 2014.
+*
+*
+*/
+
+(function() {
+	'use strict';
+
+	// MODULES //
+
+	var // xfig figure library:
+		xfig = require( './../../../lib/xfig.js' );
+
+
+	// MULTIPANEL //
+
+	/**
+	* FUNCTION: multipanel( canvas, data, width, height, left, top, labels )
+	*
+	*/
+	var multipanel = function( canvas, data, width, height, left, top, labels ) {
+		var multipanel,
+			Data = [],
+			datasets,
+			numRows, numCols,
+			graphs, area,
+			yValue = function ( d ) { return d[ 1 ]; },
+			_yMax, yMax = 0;
+
+		// [1] Instantiate a new multipanel generator and configure:
+		multipanel = xfig.multipanel( canvas )
+			.width( width )
+			.height( height )
+			.gutter( 120 )
+			.position({
+				'left': left,
+				'top': top
+			});
+
+		// [2] For each panel dataset, instantiate a new data generator and configure:
+		datasets = Object.keys( data );
+
+		numRows = datasets.length;
+		numCols = numRows;
+
+		for ( var i = 0; i < numRows; i++ ) {
+			for ( var j = 0; j < numCols; j++ ) {
+
+				Data.push(
+					xfig.data( [ data[ datasets[i] ][0], data[ datasets[j] ][0] ] )
+				);
+
+				// Compute the yMax:
+				_yMax = Data[ Data.length-1 ].max( yValue );
+				yMax = ( yMax < _yMax ) ? _yMax : yMax;
+			} // end FOR j
+		} // end FOR i
+
+		// Bind the data instance to the multipanel:
+		multipanel.data( Data )
+			.rows( numRows )
+			.cols( numCols )
+			.xMin( 0 )
+			.xMax( 1 )
+			.yMin( 0 )
+			.yMax( yMax )
+			.xLabel( 'E' )
+			.yLabel( 'density [a.u.]' )
+			.yNumTicks( 4 )
+			.headers( labels )
+			.labels( labels );
+
+		// Create the multipanel:
+		multipanel.create();
+
+		// [3] For each panel graph, instantiate a new area generator and configure:
+		graphs = multipanel.children().graph;
+		for ( var k = 0; k < graphs.length; k++ ) {
+			area = xfig.area( graphs[ k ] )
+				.labels( [ 'data 0', 'data 1' ] );
+
+			// Create the area:
+			area.create();
+		} // end FOR k
+
+	}; // end MULTIPANEL
+
+
+	// EXPORTS //
+
+	module.exports = multipanel;
+
+})();
--- app/modules/matrix/generator.js
+++ app/modules/matrix/generator.js
@@ -49,10 +49,10 @@
 		xfig = require( './../../lib/xfig.js' ),
 
 		// Module to decode dataset:
-		mapping = require( './../../utils/mapping.js' );
+		mapping = require( './../../utils/mapping.js' ),
 
 		// Chart generators:
-		// KDE = require( './charts/kde.js' );
+		Multipanel = require( './charts/multipanel.js' );
 
 
 	// GENERATOR //
@@ -62,7 +62,7 @@
 	*
 	*/
 	var generator = function( document, selection, data, clbk ) {
-		var figure, datasets, d, canvas;
+		var figure, datasets, labels = [], canvas;
 
 		// [1] Instantiate a new figure generator:
 		figure = xfig.figure();
@@ -70,30 +70,22 @@
 		// Create the figure:
 		figure.create( document, selection );
 
-		// Select the figure element and set a data attribute:
-		selection.querySelector( '.figure' )
-			.setAttribute( 'data-sortable', 1 );
+		// [2] Instantiate a new canvas generator and configure:
+		canvas = xfig.canvas( figure )
+			.width( 12000 )
+			.height( 8000 );
 
-		// [2] Get the datasets:
+		// Create the canvas:
+		canvas.create();
+
+		// [3] Decode the datasets:
 		datasets = Object.keys( data );
-
-		// [3] Create a separate canvas for each dataset and create the KDE charts:
 		for ( var i = 0; i < datasets.length; i++ ) {
+			labels.push( mapping[ datasets[ i ] ] );
+		}
 
-			d = data[ datasets[ i ] ];
-
-			// [3.0] Instantiate a new canvas generator and configure:
-			canvas = xfig.canvas( figure )
-				.width( 500 )
-				.height( 450 );
-
-			// Create the canvas:
-			canvas.create();
-
-			// [3.1] Create a new KDE chart:
-			// KDE( canvas, d, 400, 260, 90, 80, mapping[ datasets[ i ] ] );
-
-		} // end FOR i
+		// [4] Create a new Multipanel chart:
+		Multipanel( canvas, data, 11900, 7900, 90, 80, labels );
 
 		// Finished:
 		clbk();
